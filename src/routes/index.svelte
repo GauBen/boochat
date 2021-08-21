9f89f7c7a47808eaf3459b7299b03fab5b13bc37
GauBen <benaim.gautier@gmail.com>
2021-08-21 18:12:53 +0200

    feat: dark mode

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,11 +16,65 @@
   }
 </script>
 
-{#each messages as message}
-  <div>{message}</div>
-{/each}
+<main>
+  <div class="messages">
+    {#each messages as message}
+      <p>{message}</p>
+    {/each}
+  </div>
 
-<form on:submit|preventDefault={() => send()}>
-  <input type="text" bind:value />
-  <button>Envoyer</button>
-</form>
+  <form on:submit|preventDefault={() => send()}>
+    <input type="text" bind:value />
+    <button>Envoyer</button>
+  </form>
+</main>
+
+<style lang="scss">
+  :global(body) {
+    // TODO: use sanitize.css
+    margin: 0;
+    color: #fff;
+    background-color: rgb(41, 37, 65);
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .messages {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    overflow: auto;
+    padding: 0 1em;
+
+    > :first-child {
+      margin-top: auto;
+    }
+  }
+
+  form {
+    display: flex;
+    gap: 0.5em;
+    padding: 1em;
+
+    input {
+      flex: 1;
+    }
+
+    input,
+    button {
+      padding: 0.5em;
+      border-radius: 0.5em;
+      border: 1px solid rgb(24, 24, 27);
+      background: #fff;
+
+      &:focus {
+        box-shadow: 0 0 3px #fff;
+        outline: 0;
+      }
+    }
+  }
+</style>
