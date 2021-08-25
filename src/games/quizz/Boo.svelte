000642ba742232758bf19d16235693fb3fcf5fcc
GauBen <benaim.gautier@gmail.com>
2021-08-25 19:14:04 +0200

    feat: propagate settings to the game results

--- src/games/quizz/Boo.svelte
+++ src/games/quizz/Boo.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
   import type { Socket } from 'socket.io-client'
+  import { onMount } from 'svelte'
 
   export let socket: Socket | undefined = undefined
 
+  let question = ''
   let msg = '...'
+
+  onMount(() => {
+    fetch('//localhost:3001/api/game-settings')
+      .then(async (r) => r.json())
+      .then((x) => {
+        question = x.value
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  })
 
   const listen = (socket: Socket | undefined) => {
     if (!socket) return
-    socket?.on('game', (evt) => {
+    socket.on('game', (evt) => {
       msg = evt
+    })
+    socket.on('game-settings', (x) => {
+      question = x.value
     })
   }
 
   $: listen(socket)
 </script>
 
-<h1>{msg}</h1>
+<h1>{question}</h1>
+<h2>{msg}</h2>
