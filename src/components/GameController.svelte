f0b9f5c994f27e596001fbd4d37ca6fa6d3e5268
Laureηt <laurentfainsin@protonmail.com>
2021-12-15 14:12:02 +0100

    feat: better gba controller

--- src/components/GameController.svelte
+++ src/components/GameController.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { Me } from '../api'
-  import Controller from '../games/connect3/Controller.svelte'
+  import Controller from '../games/gba/Controller.svelte'
   import type { Socket } from '../socket-api'
 
   export let socket: Socket | undefined = undefined
--- src/games/gba/Controller.svelte
+++ src/games/gba/Controller.svelte
@@ -1,49 +1,79 @@
-<script>
-  const websocket = new WebSocket('ws://localhost:6789/')
+<script lang="ts">
+  import { onMount } from 'svelte'
 
-  const sendAction = (key) => () => {
-    websocket.send(JSON.stringify({ action: key }))
+  let websocket: WebSocket | undefined
+  let last_time: number = new Date().getTime()
+
+  onMount(() => {
+    websocket = new WebSocket('wss://gba.inpt.fr/')
+  })
+
+  const sendAction = (key: string) => () => {
+    if (websocket) {
+      let current_time: number = new Date().getTime()
+      if (current_time - last_time > 500) {
+        websocket.send(JSON.stringify({ action: key }))
+        last_time = current_time
+      }
+    }
   }
 </script>
 
+<title>Télécommande</title>
+
 <table class="buttons">
   <tr>
-    <td colspan="2" style="padding-bottom: 1rem;"
-      ><button id="l" on:click={sendAction('l')}>L</button></td
-    >
+    <td colspan="2" id="l">
+      <button on:click={sendAction('l')}>L</button>
+    </td>
     <td />
-    <td colspan="2" style="padding-bottom: 1rem; text-align: right;"
-      ><button id="r" on:click={sendAction('r')}>R</button></td
-    >
+    <td colspan="2" id="r">
+      <button on:click={sendAction('r')}>R</button>
+    </td>
   </tr>
+
   <tr>
     <td />
-    <td><button id="up" on:click={sendAction('up')}> ∧ </button></td>
+    <td id="up">
+      <button on:click={sendAction('up')}> ∧ </button>
+    </td>
     <td />
-    <td
-      ><button id="select" on:click={sendAction('select')}> select </button></td
-    >
-    <td><button id="start" on:click={sendAction('start')}> start </button></td>
+    <td id="select">
+      <button on:click={sendAction('select')}> select </button>
+    </td>
+    <td id="start">
+      <button on:click={sendAction('start')}> start </button>
+    </td>
   </tr>
+
   <tr>
-    <td><button id="left" on:click={sendAction('left')}> &lt; </button></td>
+    <td id="left">
+      <button on:click={sendAction('left')}> &lt; </button>
+    </td>
     <td />
-    <td style="padding-right: 3rem;"
-      ><button id="right" on:click={sendAction('right')}> > </button></td
-    >
+    <td id="right">
+      <button on:click={sendAction('right')}> > </button>
+    </td>
     <td />
     <td />
   </tr>
+
   <tr>
     <td />
-    <td><button id="down" on:click={sendAction('down')}> ∨ </button></td>
+    <td id="down">
+      <button on:click={sendAction('down')}> ∨ </button>
+    </td>
     <td />
-    <td><button id="a" on:click={sendAction('a')}>A</button></td>
-    <td><button id="b" on:click={sendAction('b')}>B</button></td>
+    <td id="a">
+      <button on:click={sendAction('a')}>A</button>
+    </td>
+    <td id="b">
+      <button on:click={sendAction('b')}>B</button>
+    </td>
   </tr>
 </table>
 
-<style>
+<style lang="scss">
   * {
     margin: 0;
     padding: 0;
@@ -53,32 +83,84 @@
     margin: auto;
     margin-top: 30vh;
   }
+  @media (orientation: landscape) {
+    button {
+      width: 10vh;
+      height: 10vh;
+    }
+
+    #start,
+    #select,
+    #l,
+    #r {
+      button {
+        width: 20vh;
+      }
+    }
+
+    #r,
+    #l {
+      padding-bottom: 10vh;
+    }
+
+    #right {
+      padding-right: 15vh;
+    }
+  }
+
+  @media (orientation: portrait) {
+    button {
+      width: 10vw;
+      height: 10vw;
+    }
+
+    #start,
+    #select,
+    #l,
+    #r {
+      button {
+        width: 20vw;
+      }
+    }
+
+    #r,
+    #l {
+      padding-bottom: 10vw;
+    }
+
+    #right {
+      padding-right: 15vw;
+    }
+  }
 
   td {
     text-align: center;
-  }
+    &#r {
+      text-align: right;
+    }
+    &#start,
+    &#select,
+    &#l,
+    &#r {
+      button {
+        border-radius: 25%;
+      }
+    }
 
-  #a,
-  #b {
-    width: 2rem;
-    height: 2rem;
-    border-radius: 50%;
-  }
+    &#left,
+    &#right,
+    &#up,
+    &#down {
+      button {
+        border-radius: 10%;
+      }
+    }
 
-  #left,
-  #right,
-  #up,
-  #down {
-    width: 2rem;
-    height: 2rem;
-    border-radius: 10%;
-  }
-
-  #start,
-  #select,
-  #l,
-  #r {
-    width: 3.5rem;
-    border-radius: 25%;
+    &#a,
+    &#b {
+      button {
+        border-radius: 50%;
+      }
+    }
   }
 </style>
