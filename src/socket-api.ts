4844d24c4d64bc87a3106236178e57591f3044a4
GauBen <benaim.gautier@gmail.com>
2022-01-30 15:50:14 +0100

    refactor: removed all game code

--- src/socket-api.ts
+++ src/socket-api.ts
@@ -18,15 +18,9 @@ export enum ServerEvent {
   DeleteMessage = 'delete message',
   RestoreMessage = 'restore message',
   LoggedOut = 'logged out',
-  GameSettings = 'game settings',
   Connected = 'connected',
-  Game = 'game',
   Stats = 'stats',
   UserUpdated = 'user updated',
-  QuestionStarts = 'question starts',
-  QuizzAnswers = 'answers',
-  QuizzLeaderboard = 'leaderboard',
-  Connect3NextTurn = 'connect3nextturn',
 }
 
 export interface ClientToServerEvents {
@@ -45,8 +39,6 @@ export interface ServerToClientEvents {
   [ServerEvent.RestoreMessage]: (id: number) => void
   [ServerEvent.LoggedOut]: () => void
   [ServerEvent.Connected]: () => void
-  [ServerEvent.GameSettings]: (settings: { value: string }) => void
-  [ServerEvent.Game]: (results: Array<[number, number]>) => void
   [ServerEvent.Stats]: (stats: {
     online: number
     connected: number
@@ -58,18 +50,6 @@ export interface ServerToClientEvents {
     }>
   }) => void
   [ServerEvent.UserUpdated]: (user: User & { team: Team }) => void
-  [ServerEvent.QuestionStarts]: (x: {
-    question: string
-    category: string
-    points: number
-    answers: string[]
-  }) => void
-  [ServerEvent.QuizzAnswers]: (x: {
-    correctAnswers: string[]
-    bestTeams: Array<Team['id']>
-    ratios: Array<[Team['id'], number]>
-  }) => void
-  [ServerEvent.QuizzLeaderboard]: (points: Array<[Team['id'], number]>) => void
 }
 
 export enum Room {
