ed02a50ec5e925911304a5014717b6c661b5e414
GauBen <benaim.gautier@gmail.com>
2022-01-30 18:25:07 +0100

    refactor: proxy api calls

---
+++ src/hooks.ts
@@ -0,0 +1,8 @@
+import type { Handle } from '@sveltejs/kit'
+
+export const handle: Handle = async ({ event, resolve }) => {
+  if (!event.url.pathname.startsWith('/api/')) return resolve(event)
+  console.log(`Proxying ${event.url.pathname}`)
+  event.url.port = import.meta.env.VITE_API_PORT
+  return fetch(event.url.toString(), event.request)
+}
